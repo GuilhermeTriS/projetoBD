b9ae9bba5153c4a22c74b025de21cb3b25a86812
GuilhermeTriS <guilherme.souza@dcomp.ufs.br>
2021-10-20 20:44:42 -0300

    ProjetoBD

---
+++ resources/views/info.blade.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Cards</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<div class ="container">
+    <h1>Cards</h1>
+    <style type="text/css">
+    .tg  {border-collapse:collapse;border-color:#93a1a1;border-spacing:0;}
+    .tg td{background-color:#fdf6e3;border-color:#93a1a1;border-style:solid;border-width:1px;color:#002b36;
+    font-family:Arial, sans-serif;font-size:14px;overflow:hidden;padding:10px 5px;word-break:normal;}
+    .tg th{background-color:#657b83;border-color:#93a1a1;border-style:solid;border-width:1px;color:#fdf6e3;
+    font-family:Arial, sans-serif;font-size:14px;font-weight:normal;overflow:hidden;padding:10px 5px;word-break:normal;}
+    .tg .tg-ul38{position:-webkit-sticky;position:sticky;text-align:left;top:-1px;vertical-align:top;will-change:transform}
+    .tg .tg-alz1{background-color:#eee8d5;text-align:left;vertical-align:top}
+    .tg .tg-0lax{text-align:left;vertical-align:top}
+    </style>
+    <table class="tg" style="undefined;table-layout: fixed; width: 289px">
+    <colgroup>
+    <col style="width: 100px">
+    <col style="width: 100px">
+    <col style="width: 100px">
+    <col style="width: 100px">
+    <col style="width: 100px">
+    <col style="width: 100px">
+    <col style="width: 100px">
+    <col style="width: 100px">
+    </colgroup>
+    <thead>
+    <tr>
+    <th class="tg-0lax">Cod</th>
+    <th class="tg-0lax">Name</th>
+    <th class="tg-0lax">Edition</th>
+    <th class="tg-0lax">Color</th>
+    <th class="tg-0lax">Type</th>
+    <th class="tg-0lax">Rarity</th>
+    <th class="tg-0lax">Qnt</th>
+    <th class="tg-0lax">Price(R$)</th>
+  </tr>
+</thead>
+<tbody>
+  <tr>
+    <td class="tg-alz1">000001</td>
+    <td class="tg-alz1">Island</td>
+    <td class="tg-alz1">8th</td>
+    <td class="tg-alz1">None</td>
+    <td class="tg-alz1">Land</td>
+    <td class="tg-alz1">Common</td>
+    <td class="tg-alz1">100</td>
+    <td class="tg-alz1">0.10</td>
+  </tr>
+  <tr>
+    <td class="tg-0lax">000002</td>
+    <td class="tg-0lax">Plains</td>
+    <td class="tg-0lax">MID</td>
+    <td class="tg-0lax">None</td>
+    <td class="tg-0lax">Land</td>
+    <td class="tg-0lax">Common</td>
+    <td class="tg-0lax">25</td>
+    <td class="tg-0lax">0.15</td>
+  </tr>
+  <tr>
+    <td class="tg-alz1">000003</td>
+    <td class="tg-alz1">Swamp(foil)</td>
+    <td class="tg-alz1">KDL</td>
+    <td class="tg-alz1">None</td>
+    <td class="tg-alz1">Land</td>
+    <td class="tg-alz1">Common</td>
+    <td class="tg-alz1">75</td>
+    <td class="tg-alz1">1.37</td>
+  </tr>
+  <tr>
+    <td class="tg-0lax">000004</td>
+    <td class="tg-0lax">Forest</td>
+    <td class="tg-0lax">KLD</td>
+    <td class="tg-0lax">None</td>
+    <td class="tg-0lax">Land</td>
+    <td class="tg-0lax">Common</td>
+    <td class="tg-0lax">66</td>
+    <td class="tg-0lax">0.12</td>
+  </tr>
+  <tr>
+    <td class="tg-alz1">000005</td>
+    <td class="tg-alz1">Mountain</td>
+    <td class="tg-alz1">JMP</td>
+    <td class="tg-alz1">None</td>
+    <td class="tg-alz1">Land</td>
+    <td class="tg-alz1">Common</td>
+    <td class="tg-alz1">400</td>
+    <td class="tg-alz1">0.19</td>
+  </tr>
+  <tr>
+    <td class="tg-0lax">000006</td>
+    <td class="tg-0lax">Black Lotus</td>
+    <td class="tg-0lax">Alpha</td>
+    <td class="tg-0lax">None</td>
+    <td class="tg-0lax">Artifact</td>
+    <td class="tg-0lax">Rare</td>
+    <td class="tg-0lax">0</td>
+    <td class="tg-0lax">&nbsp;&nbsp;&nbsp;-</td>
+  </tr>
+  <tr>
+    <td class="tg-alz1">000007</td>
+    <td class="tg-alz1">Wonder</td>
+    <td class="tg-alz1">JUD</td>
+    <td class="tg-alz1">Blue</td>
+    <td class="tg-alz1">Creature</td>
+    <td class="tg-alz1">Uncommon</td>
+    <td class="tg-alz1">40</td>
+    <td class="tg-alz1">4.53</td>
+  </tr>
+        </tbody>
+    </table>
+    <a href="/">Main</a>
+</div>
+</html>
---
+++ resources/views/main.blade.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Cards</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<div class ="container">
+    <h1>Cards</h1>
+    <style type="text/css">
+    .tg  {border-collapse:collapse;border-color:#93a1a1;border-spacing:0;}
+    .tg td{background-color:#fdf6e3;border-color:#93a1a1;border-style:solid;border-width:1px;color:#002b36;
+    font-family:Arial, sans-serif;font-size:14px;overflow:hidden;padding:10px 5px;word-break:normal;}
+    .tg th{background-color:#657b83;border-color:#93a1a1;border-style:solid;border-width:1px;color:#fdf6e3;
+    font-family:Arial, sans-serif;font-size:14px;font-weight:normal;overflow:hidden;padding:10px 5px;word-break:normal;}
+    .tg .tg-ul38{position:-webkit-sticky;position:sticky;text-align:left;top:-1px;vertical-align:top;will-change:transform}
+    .tg .tg-alz1{background-color:#eee8d5;text-align:left;vertical-align:top}
+    .tg .tg-0lax{text-align:left;vertical-align:top}
+    </style>
+    <table class="tg" style="undefined;table-layout: fixed; width: 289px">
+    <colgroup>
+    <col style="width: 100px">
+    <col style="width: 100px">
+    <col style="width: 100px">
+    </colgroup>
+    <thead>
+    <tr>
+        <th class="tg-ul38"><span style="font-weight:bold">Cod</span></th>
+        <th class="tg-ul38"><span style="font-weight:bold">Name</span></th>
+        <th class="tg-ul38"><span style="font-weight:bold">Edition</span></th>
+    </tr>
+    </thead>
+        <tbody>
+        <tr>
+            <td class="tg-alz1">000001</td>
+            <td class="tg-alz1">Island</td>
+            <td class="tg-alz1">8th</td>
+        </tr>
+        <tr>
+            <td class="tg-0lax">000002</td>
+            <td class="tg-0lax">Plains</td>
+            <td class="tg-0lax">MID</td>
+        </tr>
+        <tr>
+            <td class="tg-alz1">000003</td>
+            <td class="tg-alz1">Swamp(foil)</td>
+            <td class="tg-alz1">KDH</td>
+        </tr>
+        <tr>
+            <td class="tg-0lax">000004</td>
+            <td class="tg-0lax">Forest</td>
+            <td class="tg-0lax">KLD</td>
+        </tr>
+        <tr>
+            <td class="tg-alz1">000005</td>
+            <td class="tg-alz1">Mountain</td>
+            <td class="tg-alz1">JMP</td>
+        </tr>
+        <tr>
+            <td class="tg-0lax">000006</td>
+            <td class="tg-0lax">Black Lotus</td>
+            <td class="tg-0lax">Alpha</td>
+        </tr>
+        <tr>
+            <td class="tg-0lax">000007</td>
+            <td class="tg-0lax">Wonder</td>
+            <td class="tg-0lax">JUD</td>
+        </tr>
+        </tbody>
+    </table>
+    <a href="/info">Info</a>
+</div>
+</html>
