83d34f2895def31c5114c90604e24081d5315c17
GuilhermeTriS <86495281+GuilhermeTriS@users.noreply.github.com>
2022-04-05 18:25:20 -0300

    Update main.blade.php

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1,74 +1,57 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Cards</title>
+    <title>Ingressos</title>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
 <div class ="container">
-    <h1>Cards</h1>
+    <h1>Ingressos</h1>
     <style type="text/css">
-    .tg  {border-collapse:collapse;border-color:#93a1a1;border-spacing:0;}
-    .tg td{background-color:#fdf6e3;border-color:#93a1a1;border-style:solid;border-width:1px;color:#002b36;
-    font-family:Arial, sans-serif;font-size:14px;overflow:hidden;padding:10px 5px;word-break:normal;}
-    .tg th{background-color:#657b83;border-color:#93a1a1;border-style:solid;border-width:1px;color:#fdf6e3;
-    font-family:Arial, sans-serif;font-size:14px;font-weight:normal;overflow:hidden;padding:10px 5px;word-break:normal;}
-    .tg .tg-ul38{position:-webkit-sticky;position:sticky;text-align:left;top:-1px;vertical-align:top;will-change:transform}
-    .tg .tg-alz1{background-color:#eee8d5;text-align:left;vertical-align:top}
-    .tg .tg-0lax{text-align:left;vertical-align:top}
+    .tg  {border-collapse:collapse;border-color:#9ABAD9;border-spacing:0;}
+    .tg td{background-color:#EBF5FF;border-color:#9ABAD9;border-style:solid;border-width:0px;color:#444;
+      font-family:Arial, sans-serif;font-size:14px;overflow:hidden;padding:10px 5px;word-break:normal;}
+    .tg th{background-color:#409cff;border-color:#9ABAD9;border-style:solid;border-width:0px;color:#fff;
+      font-family:Arial, sans-serif;font-size:14px;font-weight:normal;overflow:hidden;padding:10px 5px;word-break:normal;}
+    .tg .tg-phtq{background-color:#D2E4FC;border-color:inherit;text-align:left;vertical-align:top}
+    .tg .tg-0pky{border-color:inherit;text-align:left;vertical-align:top}
     </style>
-    <table class="tg" style="undefined;table-layout: fixed; width: 289px">
-    <colgroup>
-    <col style="width: 100px">
-    <col style="width: 100px">
-    <col style="width: 100px">
-    </colgroup>
+    <table class="tg">
     <thead>
-    <tr>
-        <th class="tg-ul38"><span style="font-weight:bold">Cod</span></th>
-        <th class="tg-ul38"><span style="font-weight:bold">Name</span></th>
-        <th class="tg-ul38"><span style="font-weight:bold">Edition</span></th>
-    </tr>
+      <tr>
+        <th class="tg-0pky">Número do Ingresso</th>
+        <th class="tg-0pky">Valor do Ingresso</th>
+      </tr>
     </thead>
-        <tbody>
-        <tr>
-            <td class="tg-alz1">000001</td>
-            <td class="tg-alz1">Island</td>
-            <td class="tg-alz1">8th</td>
-        </tr>
-        <tr>
-            <td class="tg-0lax">000002</td>
-            <td class="tg-0lax">Plains</td>
-            <td class="tg-0lax">MID</td>
-        </tr>
-        <tr>
-            <td class="tg-alz1">000003</td>
-            <td class="tg-alz1">Swamp(foil)</td>
-            <td class="tg-alz1">KDH</td>
-        </tr>
-        <tr>
-            <td class="tg-0lax">000004</td>
-            <td class="tg-0lax">Forest</td>
-            <td class="tg-0lax">KLD</td>
-        </tr>
-        <tr>
-            <td class="tg-alz1">000005</td>
-            <td class="tg-alz1">Mountain</td>
-            <td class="tg-alz1">JMP</td>
-        </tr>
-        <tr>
-            <td class="tg-0lax">000006</td>
-            <td class="tg-0lax">Black Lotus</td>
-            <td class="tg-0lax">Alpha</td>
-        </tr>
-        <tr>
-            <td class="tg-0lax">000007</td>
-            <td class="tg-0lax">Wonder</td>
-            <td class="tg-0lax">JUD</td>
-        </tr>
-        </tbody>
+    <tbody>
+      <tr>
+        <td class="tg-phtq">0001</td>
+        <td class="tg-phtq">144.00</td>
+      </tr>
+      <tr>
+        <td class="tg-0pky">0002</td>
+        <td class="tg-0pky">369.00</td>
+      </tr>
+      <tr>
+        <td class="tg-phtq">0003</td>
+        <td class="tg-phtq">900.00</td>
+      </tr>
+      <tr>
+        <td class="tg-0pky">0004</td>
+        <td class="tg-0pky">625.00</td>
+      </tr>
+      <tr>
+        <td class="tg-phtq">0005</td>
+        <td class="tg-phtq">400.00</td>
+      </tr>
+      <tr>
+        <td class="tg-0pky">0006</td>
+        <td class="tg-0pky">121.00</td>
+      </tr>
+    </tbody>
+</table>
     </table>
     <a href="/info">Info</a>
 </div>
