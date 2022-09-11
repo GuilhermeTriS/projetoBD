8059ffb52062056f3bbd9f6dd00cc203cfbfa059
GuilhermeTriS <86495281+GuilhermeTriS@users.noreply.github.com>
2022-09-11 18:31:01 -0300

    Update main.blade.php

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -7,52 +7,57 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-<div class ="container">
-    <h1>Ingressos</h1>
-    <style type="text/css">
-    .tg  {border-collapse:collapse;border-color:#9ABAD9;border-spacing:0;}
-    .tg td{background-color:#EBF5FF;border-color:#9ABAD9;border-style:solid;border-width:0px;color:#444;
-      font-family:Arial, sans-serif;font-size:14px;overflow:hidden;padding:10px 5px;word-break:normal;}
-    .tg th{background-color:#409cff;border-color:#9ABAD9;border-style:solid;border-width:0px;color:#fff;
-      font-family:Arial, sans-serif;font-size:14px;font-weight:normal;overflow:hidden;padding:10px 5px;word-break:normal;}
-    .tg .tg-phtq{background-color:#D2E4FC;border-color:inherit;text-align:left;vertical-align:top}
-    .tg .tg-0pky{border-color:inherit;text-align:left;vertical-align:top}
-    </style>
-    <table class="tg">
-    <thead>
-      <tr>
-        <th class="tg-0pky">Número do Ingresso</th>
-        <th class="tg-0pky">Valor do Ingresso</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td class="tg-phtq">0001</td>
-        <td class="tg-phtq">144.00</td>
-      </tr>
-      <tr>
-        <td class="tg-0pky">0002</td>
-        <td class="tg-0pky">369.00</td>
-      </tr>
-      <tr>
-        <td class="tg-phtq">0003</td>
-        <td class="tg-phtq">900.00</td>
-      </tr>
-      <tr>
-        <td class="tg-0pky">0004</td>
-        <td class="tg-0pky">625.00</td>
-      </tr>
-      <tr>
-        <td class="tg-phtq">0005</td>
-        <td class="tg-phtq">400.00</td>
-      </tr>
-      <tr>
-        <td class="tg-0pky">0006</td>
-        <td class="tg-0pky">121.00</td>
-      </tr>
-    </tbody>
+<table>
+    <tr>
+        <td>ID do Ingresso</td>
+        <td>Tipo do Ingresso</td>
+        <td>Valor do Ingresso</td>
+    </tr>
+    <tr>
+        <td>E9</td>
+        <td>Meia</td>
+        <td>17.50</td>
+    </tr>
+    <tr>
+        <td>N11</td>
+        <td>Meia Especial</td>
+        <td>12.50</td>
+    </tr>
+    <tr>
+        <td>S12</td>
+        <td>Inteira</td>
+        <td>35.00</td>
+    </tr>
+    <tr>
+        <td>P3</td>
+        <td>Inteira Especial</td>
+        <td>30.00</td>
+    </tr>
+    <tr>
+        <td>T10</td>
+        <td>Meia</td>
+        <td>17.50</td>
+    </tr>
+    <tr>
+        <td>Y1</td>
+        <td>Inteira</td>
+        <td>35.00</td>
+    </tr>
+    <tr>
+        <td>W4</td>
+        <td>Inteira Especial</td>
+        <td>30.00</td>
+    </tr>
+    <tr>
+        <td>Q15</td>
+        <td>Meia Especial</td>
+        <td>12.50</td>
+    </tr>
+    <tr>
+        <td>R5</td>
+        <td>Meia</td>
+        <td>17.50</td>
+    </tr>
 </table>
-    </table>
     <a href="/info">Detalhes</a>
-</div>
 </html>
