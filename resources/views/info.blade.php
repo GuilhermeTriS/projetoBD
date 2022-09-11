8f09dd058a0de833b99b3b6d4151163c148fb34a
GuilhermeTriS <86495281+GuilhermeTriS@users.noreply.github.com>
2022-09-11 18:39:15 -0300

    Update info.blade.php

--- resources/views/info.blade.php
+++ resources/views/info.blade.php
@@ -1,101 +1,147 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <title>Ingressos</title>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+<style>
+table, th, td {
+  border: 1px solid black;
+  border-collapse: collapse;
+}
+th, td {
+  padding: 10px;
+  text-align: left;
+}
+#t01 {
+  width: 100%;    
+  background-color: #d7a864;
+}
+</style>
+<title>Ingressos do Cinema</title>
 </head>
-<div class ="container">
-    <h1>Ingressos</h1>
-    <style type="text/css">
-    .tg  {border:none;border-collapse:collapse;border-color:#9ABAD9;border-spacing:0;}
-    .tg td{background-color:#EBF5FF;border-color:#9ABAD9;border-style:solid;border-width:0px;color:#444;
-      font-family:Arial, sans-serif;font-size:14px;overflow:hidden;padding:10px 5px;word-break:normal;}
-    .tg th{background-color:#409cff;border-color:#9ABAD9;border-style:solid;border-width:0px;color:#fff;
-      font-family:Arial, sans-serif;font-size:14px;font-weight:normal;overflow:hidden;padding:10px 5px;word-break:normal;}
-    .tg .tg-phtq{background-color:#D2E4FC;border-color:inherit;text-align:left;vertical-align:top}
-    .tg .tg-hmp3{background-color:#D2E4FC;text-align:left;vertical-align:top}
-    .tg .tg-0pky{border-color:inherit;text-align:left;vertical-align:top}
-    .tg .tg-0lax{text-align:left;vertical-align:top}
-    </style>
-    <table class="tg">
-    <thead>
-      <tr>
-        <th class="tg-0pky">Número do Ingresso</th>
-        <th class="tg-0pky">Valor do Ingresso</th>
-        <th class="tg-0lax">Artista</th>
-        <th class="tg-0lax">Evento</th>
-        <th class="tg-0lax">Palco</th>
-        <th class="tg-0lax">Data</th>
-        <th class="tg-0lax">Horário</th>
-        <th class="tg-0lax">Quantidade</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td class="tg-phtq">0001</td>
-        <td class="tg-phtq">144.00</td>
-        <td class="tg-hmp3">Los Hermanos</td>
-        <td class="tg-hmp3">Turnê</td>
-        <td class="tg-hmp3">B</td>
-        <td class="tg-hmp3">06/08/2022</td>
-        <td class="tg-hmp3">18:15</td>
-        <td class="tg-hmp3">22</td>
-      </tr>
-      <tr>
-        <td class="tg-0pky">0002</td>
-        <td class="tg-0pky">369.00</td>
-        <td class="tg-0lax">Paramore</td>
-        <td class="tg-0lax">Lollapalooza</td>
-        <td class="tg-0lax">B</td>
-        <td class="tg-0lax">20/03/2023</td>
-        <td class="tg-0lax">20:00</td>
-        <td class="tg-0lax">170</td>
-      </tr>
-      <tr>
-        <td class="tg-phtq">0003</td>
-        <td class="tg-phtq">900.00</td>
-        <td class="tg-hmp3">System of a Down</td>
-        <td class="tg-hmp3">Rock in Rio</td>
-        <td class="tg-hmp3">A</td>
-        <td class="tg-hmp3">16/06/2022</td>
-        <td class="tg-hmp3">02:00</td>
-        <td class="tg-hmp3">1000</td>
-      </tr>
-      <tr>
-        <td class="tg-0pky">0004</td>
-        <td class="tg-0pky">625.00</td>
-        <td class="tg-0lax">Billie Eilish</td>
-        <td class="tg-0lax">Turnê</td>
-        <td class="tg-0lax">A</td>
-        <td class="tg-0lax">15/12/2022</td>
-        <td class="tg-0lax">19:45</td>
-        <td class="tg-0lax">333</td>
-      </tr>
-      <tr>
-        <td class="tg-phtq">0005</td>
-        <td class="tg-phtq">400.00</td>
-        <td class="tg-hmp3">Aurora</td>
-        <td class="tg-hmp3">Lollapalooza</td>
-        <td class="tg-hmp3">A</td>
-        <td class="tg-hmp3">11/11/2022</td>
-        <td class="tg-hmp3">21:00</td>
-        <td class="tg-hmp3">170</td>
-      </tr>
-      <tr>
-        <td class="tg-0pky">0006</td>
-        <td class="tg-0pky">121.00</td>
-        <td class="tg-0lax">Muse </td>
-        <td class="tg-0lax">Rock in Rio</td>
-        <td class="tg-0lax">B</td>
-        <td class="tg-0lax">15/06/2022</td>
-        <td class="tg-0lax">23:30</td>
-        <td class="tg-0lax">97</td>
-      </tr>
-    </tbody>
-    </table>
-    <a href="/">Listagem</a>
-</div>
+<body>
+
+<h1>Ingressos do Cinema</h1>
+
+<table id = "t01">
+    <tr>
+        <td><b>ID do Ingresso</b></td>
+        <td><b>Tipo do Ingresso</b></td>
+        <td><b>Valor do Ingresso</b></td>
+        <td><b>Filme</b></td>
+        <td><b>Gênero</b></td>
+        <td><b>Sala</b></td>
+        <td><b>Horário</b></td>
+        <td><b>Capacidade</b></td>
+        <td><b>Combo Especial</b></td>
+        <td><b>Cliente VIP</b></td>
+    </tr>
+    <tr>
+        <td>E9</td>
+        <td>Meia</td>
+        <td>17.50</td>
+        <td>Papai é Pop</td>
+        <td>Comédia</td>
+        <td>F</td>
+        <td>01:55</td>
+        <td>60</td>
+        <td>Não</td>
+        <td>Não</td>
+    </tr>
+    <tr>
+        <td>N11</td>
+        <td>Meia Especial</td>
+        <td>12.50</td>
+        <td>Men: Faces do Medo</td>
+        <td>Terror</td>
+        <td>G</td>
+        <td>04:50</td>
+        <td>25</td>
+        <td>Sim</td>
+        <td>Não</td>
+    </tr>
+    <tr>
+        <td>S12</td>
+        <td>Inteira</td>
+        <td>35.00</td>
+        <td>Pinóquio: O Menino de Madeira</td>
+        <td>Animação</td>
+        <td>E</td>
+        <td>14:40</td>
+        <td>45</td>
+        <td>Sim</td>
+        <td>Sim</td>
+    </tr>
+    <tr>
+        <td>P3</td>
+        <td>Inteira Especial</td>
+        <td>30.00</td>
+        <td>Tromba Trem</td>
+        <td>Animação</td>
+        <td>A</td>
+        <td>15:50</td>
+        <td>30</td>
+        <td>Não</td>
+        <td>Sim</td>
+    </tr>
+    <tr>
+        <td>T10</td>
+        <td>Meia</td>
+        <td>17.50</td>
+        <td>A Última Chamada</td>
+        <td>Terror</td>
+        <td>B</td>
+        <td>22:30</td>
+        <td>100</td>
+        <td>Não</td>
+        <td>Não</td>
+    </tr>
+    <tr>
+        <td>Y1</td>
+        <td>Inteira</td>
+        <td>35.00</td>
+        <td>Predestinado</td>
+        <td>Drama</td>
+        <td>C</td>
+        <td>02:25</td>
+        <td>30</td>
+        <td>Não</td>
+        <td>Não</td>
+    </tr>
+    <tr>
+        <td>W4</td>
+        <td>Inteira Especial</td>
+        <td>30.00</td>
+        <td>Um Lugar bem Longe Daqui</td>
+        <td>Drama</td>
+        <td>D</td>
+        <td>02:30</td>
+        <td>80</td>
+        <td>Sim</td>
+        <td>Não</td>
+    </tr>
+    <tr>
+        <td>Q15</td>
+        <td>Meia Especial</td>
+        <td>12.50</td>
+        <td>Homem-Aranha: Sem Volta Para Casa</td>
+        <td>Ação</td>
+        <td>H</td>
+        <td>13:55</td>
+        <td>95</td>
+        <td>Sim</td>
+        <td>Não</td>
+    </tr>
+    <tr>
+        <td>R5</td>
+        <td>Meia</td>
+        <td>17.50</td>
+        <td>Minions 2: A Origem de Gru</td>
+        <td>Animação</td>
+        <td>I</td>
+        <td>16:05</td>
+        <td>80</td>
+        <td>Sim</td>
+        <td>Sim</td>
+    </tr>
+</table>
+<a href="/">Listagem</a>
 </html>
